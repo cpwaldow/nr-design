2c7967f9596f5e9a5ce90b08ed76035d8bcf793b
Carlos Eduardo <cpwaldow@gmail.com>
2021-11-26 00:36:12 -0300

    removendo o menu e adicionando um alert após os clicks nos botoes

--- js/script.js
+++ js/script.js
@@ -22,4 +22,17 @@ if (window.SimpleSlide) {
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+const getBtn = document.querySelectorAll('.btn');
+
+function btnAlerContact() {
+  for (let index = 0; index < getBtn.length; index += 1) {
+    getBtn[index].addEventListener('click', (event) => {
+      event.preventDefault();
+      alert('Entre em contato pelo e-mail nathaliemiras2@gmail.com');
+    });
+  }
+}
+
 scrollTop();
+btnAlerContact();
